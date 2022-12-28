65880d8c732f28134081b0f76c9e495251ae27e7
unknown <nazeerahmedofficial1@gmail.com>
2022-12-28 09:30:31 +0500

    TypeError

--- 2-Variables.ts
+++ 2-Variables.ts
@@ -11,3 +11,17 @@ function varFunction()
 }
 varFunction()
 // console.log(x)
+
+// Type Checking Erros
+
+function varLetFunction()
+{
+    var myStringValuevar = "VarSting"; 
+    let myStringValuelet = "LetSTring"; // let variable is local to scope
+    if(true)
+    {
+        var myStringValuevar = 123; // error : b/c this var is same as above : so type error
+        let myStringValuelet = 1234;
+
+    }
+}
