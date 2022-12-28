28748b3a97d19c43b106d050a3a5a612ef4727c5
unknown <nazeerahmedofficial1@gmail.com>
2022-12-28 14:58:12 +0500

    Declaring Scope

--- 2-Variables.ts
+++ 2-Variables.ts
@@ -131,10 +131,30 @@ function mainFunction3() {
         innerFunction = function(param1: number) {
             return param1;
         }
-        listFunctions.push(innerFunction(i));
+        // listFunctions.push(innerFunction(i));
     }
     for (let k = 0; k < 5; k++) {
         console.log(listFunctions[k]);
     }
 }
 mainFunction3();
+
+// Declaring Scope ------------------
+let x = 1;
+console.log("Outside: " + x)
+function myFunction(){
+  // console.log("Inside before re-declaring: " + x)
+  let x = 2;
+  console.log("Inside after re-declaring: " + x)
+  if (x == 2) {
+    let x = 3;
+    console.log("Inside IF : " + x)
+    function mySecondFunction(){
+      // console.log("Inside-Inside before: " + x)
+      let x = 4;
+      console.log("Inside-Inside after: " + x)
+    }
+    mySecondFunction();
+  }
+}
+myFunction();
