622c68c680f9a7772549661dcf062a6ebd27bc0b
unknown <nazeerahmedofficial1@gmail.com>
2022-12-28 20:12:40 +0500

    Swtich scope

--- 2-Variables.ts
+++ 2-Variables.ts
@@ -158,3 +158,23 @@ function myFunction(){
   }
 }
 myFunction();
+
+
+//// Switch Scope
+function switchFunction(a: number): void {
+    switch (a) {
+        case 1:
+            let variableInCase1 = "test";
+            console.log(variableInCase1);
+            break;
+        case 2:
+            let variableInCase2 = "test2";
+            console.log(variableInCase2);
+            break;
+        default:
+          console.log("Default");    
+    }
+}
+switchFunction(1);
+switchFunction(2);
+switchFunction(3);
